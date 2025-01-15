a5cce110bfb4e1b09c667b241a692785eb74a512
João Victor Garcia <jvictor_sk8@hotmail.com>
2025-01-15 06:45:36 -0300

    Initial Commit

---
+++ SwiftUISampleApp/SwiftUISampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleAppApp.swift
+//  SwiftUISampleApp
+//
+//  Created by João Victor Garcia Leite Santana on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
